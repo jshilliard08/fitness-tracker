e2cafd174d12920204f0cc928fa565ffd31d093b
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-03-29 04:11:18 -0400

    updated post router for api.js

---
+++ Develop/routes/api.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const { query } = require("express");
+const Workout = require("../models/workout");
+
+router.post("/api/workouts", ({ body }, res) => {
+  Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
