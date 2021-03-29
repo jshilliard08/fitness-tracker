33fceac0a9b55bd6ee2f6a321ba26612108acc0a
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-03-29 04:23:57 -0400

    updating api routes

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -22,7 +22,7 @@ router.get("/api/workouts", (req, res) => {
       res.status(400).json(err);
     });
   })
-  
+
   // look up .aggregate for mongoose in order to get duration
 
   router.put("/api/workouts/:id", ({ params, body }, res) => {
@@ -37,4 +37,31 @@ router.get("/api/workouts", (req, res) => {
         res.status(400).json(err);
       });
   });
+
+  // look up .aggregate for mongoose in order to get duration range 
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([ { $addFields: { totalDuration: { $sum: "$exercises.duration" } } } ]) 
+    .limit(7)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+  })
+
+  //Delete workout
+
+  router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findbyIdAndDelete(body.id)
+      .then(() => {
+        return res.json(true);
+      })
+      .catch((err) => {
+        res.json(400).json(err);
+      });
+  });
+  
+  module.exports = router;
   
