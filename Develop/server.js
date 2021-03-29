12bc8757fe59f5580e6a6ea87cfa39321977a288
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-03-29 04:26:46 -0400

    updating mongoose connection

--- Develop/server.js
+++ Develop/server.js
@@ -8,7 +8,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Bring in Mongoose.connect
+//bring in mongoose.connect
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+// //Declare routes
+// const api = require('./routes/api');
+app.use(require("./routes/api.js"))
+
+// const html = require('./routes/html');
+app.use(require("./routes/html.js"))
+
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 
 //declare routes
 
